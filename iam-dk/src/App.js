8d418eb132b9997b24ec3cc7b7be41dd77b59262
Darren Kidd <darren.kidd@xero.com>
2019-05-29 22:10:07 +1200

    Make it pretty and make it mine

--- iam-dk/src/App.js
+++ iam-dk/src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import DarrenAvatar from './components/DarrenAvatar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <DarrenAvatar />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, my name is Darren Kidd, and I like to build things.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ iam-dk/src/components/DarrenAvatar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Avatar from 'avataaars';
+
+const DarrenAvatar = () => (
+    <Avatar
+    avatarStyle='Transparent'
+    topType='ShortHairShortWaved'
+    accessoriesType='Blank'
+    hairColor='Blonde'
+    facialHairType='Blank'
+    clotheType='Hoodie'
+    clotheColor='Blue03'
+    eyeType='Default'
+    eyebrowType='DefaultNatural'
+    mouthType='Default'
+    skinColor='Pale'
+    />
+);
+
+export default DarrenAvatar;
